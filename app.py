29204488b1da391d7ec6bb162d908668bb8a6c8f
subha <subh16322.cs@rmkec.ac.in>
2018-08-26 12:58:29 +0530

    Working
    Altered static files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request, redirect, url_for, render_template
+
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
