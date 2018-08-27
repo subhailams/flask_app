25dd91b073b75c99256861402b58c400d1c446b8
subha <subh16322.cs@rmkec.ac.in>
2018-08-27 21:51:51 +0530

    Added add_user page
    Add_user page is created and queries and models for accessing users added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Women_safety
-A web application for women safety
+# flask_movie
+A simple Flask app to follow along for my tutorial series at http://www.youtube.com/c/ChrisHawkes
+
+Thanks for visiting
--- app.py
+++ app.py
@@ -1,13 +1,52 @@
 from flask import Flask
 from flask import request, redirect, url_for, render_template
-
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:19071999@localhost/women'
+app.debug = True
+
+db=SQLAlchemy(app)
+
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+# Function to render the index template
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/profile/<email>')
+@login_required
+def profile(email):
+    user = User.query.filter_by(email=email).first()
+    return render_template('profile.html', user=user)
+
+
+# Post method to add user to database
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    user = User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
